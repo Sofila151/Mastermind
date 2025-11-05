c468d6825157dec1a63722b119d1d2393c4238cb
Sofila151 <bustams2@my.erau.edu>
2025-11-04 19:17:37 -0700

    Main game play Area
    User will input user name, read the rules, and then the game will prompt the user to select colors for each peg.

---
+++ userInputs.py
@@ -0,0 +1,23 @@
+#User Inputs For Guesses
+import gameFunctions as F
+import gameRules as G
+
+file = open("Game_Rules.txt","r")
+
+file.read()
+
+file.close()
+
+import create_board as B
+
+gameBoard = B.create_board()
+gameColors = B.fillColors(t)
+
+
+
+
+    
+
+
+
+
