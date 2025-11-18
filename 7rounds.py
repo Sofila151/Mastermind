07f88d58935918750a86d9da5300f628147b76d3
quinnd1 <QUINND1@my.erau.edu>
2025-11-18 11:40:35 -0700

    Add files via upload

---
+++ 7rounds.py
@@ -0,0 +1,16 @@
+import guess_checker_function as gcf
+import random_generation as gen
+
+color_code = gen.number_generator()
+print(color_code)
+
+for i in range(7):
+    print(f"Round {i+1}")
+    result = gcf.guess_checking(color_code)
+    pegs = result[0]
+    guesses = result[1]
+    if "white" not in pegs and "blank" not in pegs:
+        print("Congratulations!!!\n You solved the Secret Code!")
+        break
+    elif "white" in pegs or "blank" in pegs:
+        print("Looks like you're still missing some of the code! \n Try again!")
