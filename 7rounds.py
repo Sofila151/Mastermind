027ebb70bb4f6ae7dee4f0cfab95f9e51a024ef0
quinnd1 <QUINND1@my.erau.edu>
2025-11-18 11:48:02 -0700

    Update 7rounds.py

--- 7rounds.py
+++ 7rounds.py
@@ -2,19 +2,29 @@ import guess_checker_function as gcf
 import random_generation as gen
 import create_board as cb
 import gameRules as gr
+import turtle as t
 
+file = open("gameRules.txt","r")
 
+file.read()
+
+file.close()
+
+game_board = cb.create_board()
 
 color_code = gen.()
 
-for i in range(7):
-    print(f"Round {i+1}")
+for round_num in range(1,8):
+    print(f"Round {round_num}")
     result = gcf.guess_checking(color_code)
     pegs = result[0]
     guesses = result[1]
+    for i in range(3):
+        gameColors = B.fillColors(t,guesses[i],round_num)
     if "white" not in pegs and "blank" not in pegs:
         print("Congratulations!!!\n You solved the Secret Code!")
         break
     elif "white" in pegs or "blank" in pegs:
         print("Looks like you're still missing some of the code! \n Try again!")
+
 
