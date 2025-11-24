f6828ee6337cf7bf0c7cc75fc811a8d328285a50
Sofila151 <bustams2@my.erau.edu>
2025-11-24 10:18:17 -0700

    Update

--- gameFunctions.py
+++ gameFunctions.py
@@ -2,7 +2,7 @@
 #Main Contributor to File: Sofia
 import random
 
-def play_again():n
+def play_again():#Nyx Did Version 1
   """This function asks the player if they would like to begin a new game round"""
   answer = input("Do you want to play again? (y/n): ").lower()
   if answer == "y":
